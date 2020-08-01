537711ce3e8edc39fa6ac421dfacd9994f17fdfa
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-31 21:36:24 -0300

    Criação de um elemento link com React

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+function App() {
+  return React.createElement(
+    'a',
+    { href: 'https://www.origamid.com' },
+    'Origamid',
+  );
+}
+
+ReactDOM.render(App(), document.getElementById('root'));
