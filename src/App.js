b76066e4fbb74e6ef58a2752761aa95fb2af0616
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-31 22:12:50 -0300

    Alteraçao dos arquivos após instalação do Babel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 const App = () => {
-  return React.createElement(
-    'a',
-    { href: 'https://www.origamid.com' },
-    'Origamid',
-  );
+  return <a href="https://www.origamid.com">Origamid</a>;
 };
 
 export default App;
